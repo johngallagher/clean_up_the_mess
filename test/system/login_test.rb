572f4f7a7cae0fd4ad0db0162e24b329359e1fba
John Gallagher <john@synapticmishap.co.uk>
2023-06-03 14:49:53 +0100

    wip: failing happy path test
    We want a happy path test that makes sure the Castle token is being
    injected into the form.
    
    It's not, so we get:
    
    ```
    Error:
    LoginTest#test_attempting_login_when_user_is_activated_shows_the_user's_welcome_page:
    Castle::InvalidRequestTokenError: Invalid Request Token value:
    request_token is invalid
        app/controllers/sessions_controller.rb:44:in `user_is_a_hacker?'
        app/controllers/sessions_controller.rb:34:in `user_is_genuine?'
        app/controllers/sessions_controller.rb:8:in `create'
    ```
    
    This is what we expect. Next step - get this passing.
    
    @practice:tdd @failing @test_desiderata:inspires_confidence

--- test/system/login_test.rb
+++ test/system/login_test.rb
@@ -1,15 +1,15 @@
 require "application_system_test_case"
 
 class LoginTest < ApplicationSystemTestCase
-  test "attempting login when user is not activated " \
-       "tells the user to activate their account first" do
-    user = FactoryBot.create :user, name: "Joe Bloggs", email: 'joe@example.org', password: 'password'
+  test "attempting login when user is activated " \
+       "shows the user's welcome page" do
+    user = FactoryBot.create :user, name: "Joe Bloggs", email: 'joe@example.org', password: 'password', activated: true, activated_at: Time.zone.now
     visit login_path
 
     fill_in 'Email', with: 'joe@example.org'
     fill_in 'Password', with: 'password'
     click_button 'Log in'
 
-    assert_text "Account not activated"
+    assert_text "Joe Bloggs"
   end
 end
