136c263be1f28428855a62d486242333b70b08e5
John Gallagher <john@synapticmishap.co.uk>
2023-06-04 15:18:58 +0100

    wip: failing test for registration attempted
    Use the helper we had in the other test.
    
    Copying it across results in duplication, but we focus on making
    progress right now.
    
    We can pull these test helpers out into a shared module or class later.
    
    Decent error message:
    
    ```
    Failure:
    UsersSignupTest#test_when_the_user_has_posted_a_signup_form_notify_fraud_detection_that_registration_has_been_attempted [/Users/johngallagher/Documents/Projects/sample_rails_app/test/integration/users_signup_test.rb:57]:
    The request POST https://api.castle.io/v1/filter with given block was expected to execute 1 time but it executed 0 times
    
    The following requests were made:
    
    No requests were made.
    ```
    
    @design_practice:tdd__failing_test @test_quality:readable
    @test_quality:obvious_failures

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -48,7 +48,28 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test 'when the user has posted a signup form ' \
        'notify fraud detection that registration has been attempted' do
-    skip
+    sign_up_as(
+      name: 'Example User',
+      email: 'user@example.com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+    assert_fraud_detection_notified_of_registration_attempted_with(
+      params: {
+        email: 'user@example.com'
+      },
+      context: {
+        ip: '127.0.0.1',
+        headers: {
+          "Content-Length": '150',
+          "Remote-Addr": '127.0.0.1',
+          Version: 'HTTP/1.0',
+          Host: 'www.example.com',
+          Accept: 'text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
+          Cookie: true
+        }
+      }
+    )
   end
 
   test 'when the user signs up and they already exist ' \
@@ -57,19 +78,51 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
 
   test 'when the user signs up with a high likelihood of being a hacker' \
-       "notify fraud detection that registration has succeeded " \
+       'notify fraud detection that registration has succeeded ' \
        'and block them from signing up' do
     skip
   end
 
   test 'when the user signs up with a low likelihood of being a hacker' \
-       "notify fraud detection that registration has succeeded " \
-       "and sign them up" do
+       'notify fraud detection that registration has succeeded ' \
+       'and sign them up' do
     skip
   end
 
   test 'when the user signs up with a high likelihood of being a hacker' \
        'block them from signing up' do
     skip
+  end
+
+  private
+
+  def sign_up_as(
+    name:,
+    email:,
+    password: 'password',
+    password_confirmation: password,
+    likelihood_of_being_a_hacker: 0.0
+  )
+    VCR.use_cassette("sign_up_user_with_hacker_risk_#{likelihood_of_being_a_hacker}_with_email_#{email}") do
+      get signup_path
+      post users_path, params: {
+        castle_request_token: "test|device:chrome_on_mac|risk:#{likelihood_of_being_a_hacker}",
+        user: {
+          name: name,
+          email: email,
+          password: password,
+          password_confirmation: password_confirmation
+        }
+      }
+    end
+  end
+
+  def assert_fraud_detection_notified_of_registration_attempted_with(properties)
+    assert_requested(:post, 'https://api.castle.io/v1/filter') do |request|
+      parsed_body = JSON.parse(request.body).deep_symbolize_keys
+      return false if parsed_body.slice(:type, :status) != ({ type: '$registration', status: '$attempted' })
+
+      assert_equal parsed_body.slice(*properties.keys), properties.deep_symbolize_keys
+    end
   end
 end
