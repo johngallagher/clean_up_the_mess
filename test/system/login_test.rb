7e640073c0ab8c93942dec1707690e611e12d5e6
John Gallagher <john@synapticmishap.co.uk>
2023-06-04 12:11:30 +0100

    wip: write high level test we want
    We write the test we want.
    
    What did we expect?
    
    `#log_in_as` should have been unrecognised.
    
    Instead, we get a strange error message:
    
    ```
    Error:
    LoginTest#test_attempting_login_shows_the_user's_welcome_page:
    NameError: undefined local variable or method `session' for #<LoginTest:0x0000000105208a90>
        test/test_helper.rb:15:in `log_in_as'
        test/system/login_test.rb:6:in `block in <class:LoginTest>'
    ```
    
    Why? Because we're coupled to the implementation provided by the superclass.
    
    We can live with this for now by overriding this method with a private
    method, but you can see the drawbacks of coupling.
    
    It's not clear how to fix this quickly.
    
    @antipattern:tight_coupling @design_metric:coupling
    @design_practice:programming_by_intent

--- test/system/login_test.rb
+++ test/system/login_test.rb
@@ -1,18 +1,9 @@
 require 'application_system_test_case'
 
 class LoginTest < ApplicationSystemTestCase
-  test 'attempting login when user is activated ' \
-       "shows the user's welcome page" do
-    VCR.use_cassette('login_with_low_risk_user') do
-      user = FactoryBot.create :user, name: 'Joe Bloggs', email: 'joe@example.org', password: 'password',
-                                      activated: true, activated_at: Time.zone.now
-      visit login_path
-
-      fill_in 'Email', with: 'joe@example.org'
-      fill_in 'Password', with: 'password'
-      click_button 'Log in'
-
-      assert_text 'Joe Bloggs'
-    end
+  test "attempting login shows the user's welcome page" do
+    user = FactoryBot.create(:user, name: 'Joe Bloggs')
+    log_in_as(user)
+    assert_on_welcome_page_for(user)
   end
 end
