3a4f2c810059c73c48e9d5d58337d2f9be5927be
John Gallagher <john@synapticmishap.co.uk>
2023-06-06 22:38:12 +0100

    chore: remove dead code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,12 +5,10 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   fixtures :all
 
-  # Returns true if a test user is logged in.
   def is_logged_in?
     !session[:user_id].nil?
   end
 
-  # Log in as a particular user.
   def log_in_as(user)
     session[:user_id] = user.id
   end
