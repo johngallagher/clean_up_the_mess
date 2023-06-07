b3a329a0090ea3c0f948963bde98866f2cc09856
John Gallagher <john@synapticmishap.co.uk>
2023-06-07 21:08:44 +0100

    doc: concerns

--- app/controllers/concerns/protectable.rb
+++ app/controllers/concerns/protectable.rb
@@ -53,51 +53,43 @@ module Protectable
   end
 
   included do
-    def assess_risk_of_a_bad_actor_logging_in(user:)
-      response = fetch_hacker_likelihood(user: user, type: '$login', status: '$succeeded')
-      RiskScore.new(response[:risk])
-    end
-      
-    def assess_risk_of_a_bad_actor_registering(user:)
-      response = fetch_hacker_likelihood(user: user, type: '$registration', status: '$succeeded')
-      RiskScore.new(response[:risk])
-    end
-
-    def assess_risk_of_a_bad_actor_creating_a_micropost(user:)
-      response = fetch_hacker_likelihood(user: user, type: '$custom', name: 'Created a micropost')
-      RiskScore.new(response[:risk])
-    end
-
+    # Match policy, login
     def match_actor_against_policy_for_logging_in(user:)
       response = fetch_hacker_likelihood(user: user, type: '$login', status: '$succeeded')
       RiskPolicy.new(response[:policy])
     end
 
+    # Match policy, registration
     def match_actor_against_policy_for_registration(user:)
       response = fetch_hacker_likelihood(user: user, type: '$registration', status: '$succeeded')
       RiskPolicy.new(response[:policy])
     end
 
+    # Match policy, micropost
     def match_actor_against_policy_for_creating_a_micropost(user:)
       response = fetch_hacker_likelihood(user: user, type: '$custom', name: 'Created a micropost')
       RiskPolicy.new(response[:policy])
     end
 
+    # Protecting, micropost
     def protect_creating_a_micropost_from_bad_actors(user:, request:)
       policy = match_actor_against_policy_for_creating_a_micropost(user: user)
       block_or_challenge_bad_actors(policy: policy, request: request)
     end
 
+    # Protecting, login
     def protect_login_from_bad_actors(user:, request:)
       policy =  match_actor_against_policy_for_logging_in(user: user)
       block_or_challenge_bad_actors(policy: policy, request: request)
     end
 
+    # Protecting, registration
     def protect_registration_from_bad_actors(user:, request:)
       policy =  match_actor_against_policy_for_registration(user: user)
       block_or_challenge_bad_actors(policy: policy, request: request)
     end
 
+    # blocking or challenging
     def block_or_challenge_bad_actors(policy:, request:)
       action = PolicyAction.new(policy: policy)
       action.on_deny { block_ip_address(request.remote_ip) }
@@ -105,6 +97,7 @@ module Protectable
       action
     end
 
+    # Castle
     def fetch_hacker_likelihood(user:, type:, status: '', name: '')
       castle = ::Castle::Client.new
       token = params[:castle_request_token]
@@ -127,22 +120,27 @@ module Protectable
       response
     end
 
+    # Fraud, registration
     def notify_fraud_detection_system_of_registration_failed
       notify_fraud_detection_system_of(type: '$registration', status: '$failed')
     end
 
+    # Fraud, registration
     def notify_fraud_detection_system_of_registration_attempted
       notify_fraud_detection_system_of(type: '$registration', status: '$attempted')
     end
 
+    # Fraud, login
     def notify_fraud_detection_system_of_login_attempted
       notify_fraud_detection_system_of(type: '$login', status: '$attempted')
     end
 
+    # Fraud, login
     def notify_fraud_detection_system_of_failed_login_attempt
       notify_fraud_detection_system_of(type: '$login', status: '$failed')
     end
 
+    # Castle
     def notify_fraud_detection_system_of(type:, status:)
       email = user_params[:email] if type == '$registration'
       email = params[:session][:email] if type == '$login'
@@ -164,14 +162,17 @@ module Protectable
       )
     end
 
+    # firewall, IP address
     def challenge_ip_address(ip)
       create_firewall_rule(ip, mode: 'challenge')
     end
 
+    # firewall, IP address
     def block_ip_address(ip)
       create_firewall_rule(ip, mode: 'block')
     end
 
+    # Cloudflare, firewall
     def create_firewall_rule(ip, mode:)
       require 'uri'
       require 'net/http'
